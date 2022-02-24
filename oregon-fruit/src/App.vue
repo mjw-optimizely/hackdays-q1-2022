f7a2ca5e62fecc1e2366f0df6906b4ade01a1e7e
Mike Waldron <91098610+mjw-optimizely@users.noreply.github.com>
2022-02-24 09:48:18 -0700

    I don't think this is going to work as a Vue.js app. Maybe as a standard Node.js app.

--- oregon-fruit/src/App.vue
+++ oregon-fruit/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <CmdCmd/>
+  <CmdCmd cmd="ls -y"/>
 </template>
 
 <script>
--- oregon-fruit/src/components/Cmd.vue
+++ oregon-fruit/src/components/Cmd.vue
@@ -1,22 +1,50 @@
 <template>
   <div class="cmd">
     <button v-on:click="meow">Meow</button>
+    <textarea ref="stdout"/>
+    <textarea ref="stderr"/>
+    <textarea ref="msgs"/>
   </div>
 </template>
 
 <script>
+// https://stackoverflow.com/questions/54459442/module-not-found-error-cant-resolve-child-process-how-to-fix
+// import ChildProcess from "child_process";
+// const { spawn } = require('child_process');
+import { spawn } from 'child_process'
+
 export default {
-  name: 'CmdCmd',
+  name: "CmdCmd",
+  components: {
+    // ChildProcess,
+  },
   props: {
-    msg: String
+    cmd: String,
   },
   methods: {
-    meow : function() {
-      alert('Meow')
-    }
-  }
-}
+    meow: function () {
+      this.$refs.msgs.textContent = "Cat's meowing, dude.\n"
+      const ls = spawn(this.cmd)
+
+      ls.stdout.on("data", (data) => {
+        this.$refs.stdout.textContent = 'stdout: ' + data;
+      });
+
+      ls.stderr.on("data", (data) => {
+        this.$refs.stderr.textContent = 'stderr: ' + data;
+      });
+
+      ls.on("close", (code) => {
+        this.$refs.msgs.textContent += 'child process exited with code ' + code
+      });
+      this.$refs.msgs.textContent = "Yep.\n"
+    },
+  },
+};
 </script>
 
 <style scoped>
+textarea {
+  width: 100%;
+}
 </style>
